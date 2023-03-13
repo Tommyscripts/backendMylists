b552a06c1597925e4eb2637830cc6191643f1ffd
Tommyscripts <dark.angel.alhue@gmail.com>
2023-03-13 08:54:23 +0000

    Updated: createProducts only for admin

--- api/controllers/products.controller.js
+++ api/controllers/products.controller.js
@@ -2,7 +2,8 @@ const ProductsModel = require("../models/products.model");
 const bcrypt = require("bcrypt");
 
 module.exports = {
-  addProduct
+  addProduct,
+  createProduct,
 };
 
 
@@ -10,5 +11,17 @@ function addProduct(req, res) {
     ProductsModel.findById(res.locals.user.id)
       .populate({path:"productos",populate:{path:"list"}})
       .then((result) => res.json(result.favorites))
+      .catch((err) => res.json(err));
+  }
+
+  function createProduct(req, res) {
+    const products = res.locals.product;
+    if (req.body.password) {
+      req.body.password = bcrypt.hashSync(req.body.password, 10);
+    }
+    ProductsModel.findByIdAndUpdate(products, req.body, {
+      new: true,
+    })
+      .then((response) => res.json(response))
       .catch((err) => res.json(err));
   }
--- api/routers/products.router.js
+++ api/routers/products.router.js
@@ -3,9 +3,13 @@ const { authUser, adminCheck } = require("../utils"); // Authenticated Route
 
 const {
   addProduct,
+  createProduct,
 } = require("../controllers/products.controllers");
 
 
 router.get("/products", authUser, addProduct);
+router.post("/products", adminCheck, createProduct);
+
+
 
 module.exports = router;
