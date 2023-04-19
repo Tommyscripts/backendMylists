f0d5917f2e684137a2fc92f858df9eb848c0ff9b
Tommyscripts <dark.angel.alhue@gmail.com>
2023-04-19 08:52:46 +0100

    Updated: List router, added rolecheck

--- api/routers/lists.router.js
+++ api/routers/lists.router.js
@@ -9,8 +9,8 @@ const {
 } = require("../controllers/list.controllers");
 
 
-router.get("/list", authUser,getList)
-router.post("/list", authUser, createList);
-router.patch("/list", authUser, updateList);
-router.delete("/list/:id", authUser, deleteListById);
+router.get("/list", authUser,roleCheck, getList)
+router.post("/list", authUser,roleCheck, createList);
+router.patch("/list", authUser,roleCheck, updateList);
+router.delete("/list/:id", authUser,roleCheck, deleteListById);
 module.exports = router;
