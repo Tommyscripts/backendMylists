b38e2a1e6295822965c08de344f45cba517e8e82
Tommyscripts <dark.angel.alhue@gmail.com>
2023-04-24 09:34:41 +0100

    update: Created add remove and get products in list for "favorite"

--- api/controllers/users.controllers.js
+++ api/controllers/users.controllers.js
@@ -1,22 +1,24 @@
 const UserModel = require("../models/user.model");
-const ListModel = require("../models/list.model")
+const ListModel = require("../models/list.model");
 const bcrypt = require("bcrypt");
 
 module.exports = {
   getUserById,
   deleteUserById,
   updateUser,
-  createUser
+  createUser,
+  createListsProducts,
+  getListsProducts,
+  updateListsProducts
 };
 
 function getUserById(req, res) {
   const users = res.locals.user;
   UserModel.findById(users.id)
-  .populate("listas")
-  .then((response)=> res.json(response))
-  .catch((err)=> res.json(err))
- }
-
+    .populate("listas")
+    .then((response) => res.json(response))
+    .catch((err) => res.json(err));
+}
 
 function updateUser(req, res) {
   const users = res.locals.user;
@@ -42,12 +44,40 @@ function createUser(req, res) {
   }
   UserModel.create(req.body)
     .then((user) => {
-    ListModel.findOne({name:"Todos los productos"})
-        .then((res)=>{
-          user.listas.push(res._id.toLocaleString())
-          user.save(),
-          res.json(user)
-        }).catch((err) => res.json(err));
+      ListModel.findOne({ name: "Todos los productos" })
+        .then((res) => {
+          user.listas.push(res._id.toLocaleString());
+          user.save(), res.json(user);
+        })
+        .catch((err) => res.json(err));
     })
+    .catch((err) => res.json(err));
+}
+
+function getListsProducts(req, res) {
+  UserModel.findById(res.locals.user.id)
+    .populate({ path: "favorites", populate: { path: "restaurant" } })
+    .then((result) => res.json(result.favorites))
+    .catch((err) => res.json(err));
+}
+
+function createListsProducts(req, res) {
+  UserModel.findById(res.locals.user.id)
+    .then((result) => {
+      result.favorites.push(req.body.id);
+      result.save().then((fav) => {
+        res.json(result.favorites);
+      });
+    })
+    .catch((err) => res.json(err));
+}
+function updateListsProducts(req, res) {
+  UserModel.findById(res.locals.user.id)
+  .then(user => {
+    let index = user.favorites.indexOf(req.body.id)
+    user.favorites.splice(index, 1)
+    user.save()
+    res.json(user)
+  })
     .catch((err) => res.json(err));
 }
--- api/routers/users.router.js
+++ api/routers/users.router.js
@@ -5,11 +5,17 @@ const {
   getUserById,
   deleteUserById,
   updateUser,
-  createUser
+  createUser,
+  getListsProducts,
+  createListsProducts,
+  updateListsProducts
 } = require("../controllers/users.controllers");
 
 router.get("/profile", authUser, getUserById);
 router.post('/admin',authUser,adminCheck,createUser);
 router.put("/profile",authUser, updateUser);
 router.delete("/profile", authUser, deleteUserById);
+router.get("/lista/producto",authUser,getListsProducts)
+router.create("/lista/add",authUser,createListsProducts)
+router.path("/lista/remove",authUser,updateListsProducts)
 module.exports = router;
