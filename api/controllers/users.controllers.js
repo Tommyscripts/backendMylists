efc5ba639866cbd4d19f0a466bd3b0b96ebb6a7a
Tommyscripts <dark.angel.alhue@gmail.com>
2023-05-10 11:15:52 +0100

    fixed update

--- api/controllers/users.controllers.js
+++ api/controllers/users.controllers.js
@@ -78,18 +78,19 @@ function createListAdd(req, res) {
 
 function updateListaRemoveCasa(req, res) {
   const users = res.locals.user;
+  console.log(req.params)
   UserModel.findById(users.id)
     .populate({ path: "listas", populate: { path: "productos" } })
     .then((user) => {
-      ListModel.find({ name: "Lista de casa" })
+      ListModel.findById( req.params.lista )
         .then((list) => {
-          let index = list[0].productos.indexOf(req.params.id);
-          list[0].productos.splice(index, 1);
-          list[0].save();
-          ListModel.find({ name: "Lista de compra" })
+          let index = list.productos.indexOf(req.params.id);
+          list.productos.splice(index, 1);
+          list.save();
+          ListModel.findById(req.params.compra)
             .then((list) => {
-              list[0].productos.push(req.params.id);
-              list[0].save();
+              list.productos.push(req.params.id);
+              list.save();
               res.json(user);
             })
             .catch((err) => res.json(err));
@@ -102,26 +103,27 @@ function updateListaRemoveCasa(req, res) {
 
 function updateListaRemoveCompra(req, res) {
   const users = res.locals.user;
+
   UserModel.findById(users.id)
     //hago el populate para poder entrar a productos de la lista del usuario
     .populate({ path: "listas", populate: { path: "productos" } })
     .then((user) => {
       //le digo a que lista quiero entrar y cual quiero modificar
-      ListModel.find({ name: "Lista de compra" })
+      ListModel.findById( req.params.lista )
         .then((list) => {
           //encuentra el indice del producto dentro del array de producto que tiene listas
-          let index = list[0].productos.indexOf(req.params.id);
+          let index = list.productos.indexOf(req.params.id);
           //le pido que borre el producto indicado en dicha array, es necesario darle la posicion 0 para que busque la ID del producto dseado
-          list[0].productos.splice(index, 1);
+          list.productos.splice(index, 1);
           //una vez borrado hago que se guarden los datos
-          list[0].save();
+          list.save();
           //accedo a la lista que quiero meter el producto una vez eliminado
           ListModel.find({ name: "Lista de casa" })
             .then((list) => {
               //puseo la id del producto que he eliminado anteriormente
-              list[0].productos.push(req.params.id);
+              list.productos.push(req.params.id);
               //una vez borrado hago que se guarden los datos
-              list[0].save();
+              list.save();
               res.json(user);
             })
             .catch((err) => res.json(err));
